13e0d47a3262651f7e885926422e940a569e1226
Prakhar <ptofficial29@gmail.com>
2023-01-15 23:23:23 +0530

    img issue fixed

--- src/components/RotatingDesign.jsx
+++ src/components/RotatingDesign.jsx
@@ -3,25 +3,21 @@ import design from "../assets/design.png"
 const RotatingDesign = ({type}) => {
     const img = new Image();
     img.src = design;
-    img.onload = () => {
-        console.log(img.height);
-        console.log(img.width);
-      };
       const type1 = {
-        left:-1 * img.width /3,
-        top:-1 * img.height / 3,
+        left:-1 * 500 /3,
+        top:-1 * 500 / 3,
       }
       const type2 = {
-        left:-1 * img.width /3,
-        bottom:-1 * img.height / 3,
+        left:-1 * 500 /3,
+        bottom:-1 * 500 / 3,
       }
       const type3 = {
-        right:-2 * img.width /3,
-        top:-1 * img.height / 3,
+        right:-2 * 500 /3,
+        top:-1 * 500 / 3,
       }
       const type4 = {
-        right:-2 * img.width /3,
-        bottom:-1 * img.height / 3,
+        right:-2 * 500 /3,
+        bottom:-1 * 500 / 3,
       }
       const selectedType = type==1?type1:type==2?type2:type==3?type3:type==4?type4:'';
 
